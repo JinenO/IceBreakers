4cef104b57a8528a20eed46c927f51a9b4fc9ce2
Gee0410 <sinrou0410@gmail.com>
2026-02-09 12:10:56 +0800

    feat:add submenu data and js folder

---
+++ frontend/js/config.js
@@ -0,0 +1,26 @@
+const SUB_MENU_DATA = {
+    'needs': [
+        { id: 'water', label: 'WATER', sub: 'I am thirsty', icon: 'water.png' },
+        { id: 'food', label: 'FOOD', sub: 'I am hungry', icon: 'food.png' },
+        { id: 'toilet', label: 'TOILET', sub: 'Need assistance', icon: 'toilet.png' },
+        { id: 'medicine', label: 'MEDICINE', sub: 'Pain/Meds', icon: 'medicine.png' },
+        { id: 'help', label: 'ASSIST', sub: 'Call Caregiver', icon: 'help.png' },
+        { id: 'back', label: 'BACK', sub: 'Main Menu', icon: 'back.png' }
+    ],
+    'body': [
+        { id: 'roll', label: 'ROLL OVER', sub: 'Change position', icon: 'roll.png' },
+        { id: 'head', label: 'LIFT HEAD', sub: 'Adjust pillow', icon: 'head.png' },
+        { id: 'itch', label: 'ITCHY', sub: 'Scratch body', icon: 'itch.png' },
+        { id: 'temp', label: 'TEMP', sub: 'Hot / Cold', icon: 'temp.png' },
+        { id: 'massage', label: 'MASSAGE', sub: 'Muscle stiff', icon: 'massage.png' },
+        { id: 'back', label: 'BACK', sub: 'Main Menu', icon: 'back.png' }
+    ],
+    'media': [
+        { id: 'tv', label: 'TV', sub: 'Watch News', icon: 'tv.png' },
+        { id: 'music', label: 'MUSIC', sub: 'Play Relax', icon: 'music.png' },
+        { id: 'photos', label: 'ALBUM', sub: 'Family photos', icon: 'photos.png' },
+        { id: 'read', label: 'READ', sub: 'Audio Book', icon: 'read.png' },
+        { id: 'news', label: 'YOUTUBE', sub: 'Video content', icon: 'youtube.png' },
+        { id: 'back', label: 'BACK', sub: 'Main Menu', icon: 'back.png' }
+    ]
+};
